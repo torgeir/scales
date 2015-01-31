a4126dbfda739f2d5f4ae3fe77d5e816d0144b5b
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-01-31 22:50:31 +0100

    Destructure children.

--- scripts/button.js
+++ scripts/button.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import component from 'omniscient';
 
-export default component(function Button ({className, onClick}) {
-  return <button className={className} onClick={onClick}>{this.props.children}</button>;
-}).jsx;
+export default component(({className, onClick, children}) =>
+  <button className={className} onClick={onClick}>{children}</button>
+).jsx;
